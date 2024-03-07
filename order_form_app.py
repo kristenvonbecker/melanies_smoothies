7b29d4606f7682e7c1e347e461b841341b8e0696
Kristen Beck <kristenvonbecker@gmail.com>
2024-03-06 21:45:13 -0800

    Update order_form_app.py
    Add search_on column and Pandas

--- order_form_app.py
+++ order_form_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col 
 import requests
+import pandas as pd
 
 st.title(" :cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want to customize your smoothie...")
@@ -12,6 +13,11 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:', 
@@ -26,6 +32,9 @@ if ingredients_list:
     for ingredient in ingredients_list:
         
         ingredient_string += ingredient + ', '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(ingredient + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + ingredient)
