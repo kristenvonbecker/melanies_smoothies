c0e7fc418138c9a8cbc331e60d11ed0d54499256
Kristen Beck <kristenvonbecker@gmail.com>
2024-03-06 20:27:36 -0800

    Update order_form_app.py
    Connected changes needed for move from SiS to SniS

--- order_form_app.py
+++ order_form_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col 
 
 st.title(" :cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -8,7 +7,9 @@ st.write("Choose the fruits you want to customize your smoothie...")
 name_on_order = st.text_input('Name on smoothie:')
 st.write('The name on your smoothie will be:', name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 
 ingredients_list = st.multiselect(
